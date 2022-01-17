f6a27e72e5c75b6b4c0236e03ec77297b5d190d8
Michael Knutson <mrknutson44@gmail.com>
2022-01-17 09:49:47 -0900

    init

---
+++ client/src/Memes.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Memes = ()=>{
+  const [memes, setMemes]=useState([])
+
+  useEffect(()=>{
+    getMemes()
+  },[])
+
+  const getMemes = async () =>{
+    try{
+      let response = await axios.get("/api/memes")
+      setMemes(response.data)
+    } catch (err) {
+      console.log("error getting Memes")
+    }
+  };
+
+  return(
+    <div>
+      <h1>Memes</h1>
+      <p>{JSON.stringify(memes)}</p>
+    </div>
+  )
+}
+
+export default Memes;
